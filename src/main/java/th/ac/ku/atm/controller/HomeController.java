e74cb36caff14de3f504ad717b867ded47eda189
KornSiwat <siwat@siwat.dev>
2020-09-23 23:09:39 +0700

    implemented redirect base path to home

--- src/main/java/th/ac/ku/atm/controller/HomeController.java
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping(value = {"/home", "/"})
     public String getHomePage(Model model) {
         model.addAttribute("greeting", "Sawaddee");
         // return home.html
